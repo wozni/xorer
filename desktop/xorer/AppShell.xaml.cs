f98933dd51033655a4b4b863b0700aae740c71be
Oliwier <walkowiakoliwier1@gmail.com>
2026-03-30 08:10:58 +0200

    Dodanie wersji desktopowej z interfejsem

---
+++ desktop/xorer/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace xorer
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ desktop/xorer/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace xorer
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
